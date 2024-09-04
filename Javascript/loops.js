34246620663df05e0e7d3491ba07ad4e339baf6a
chicken560 <braydondavis07@gmail.com>
2024-09-04 09:27:38 -0600

    Add files via upload

---
+++ Javascript/loops.js
@@ -0,0 +1,17 @@
+console.log("Theater Seating Arrangement:");
+for (let rows = 1; rows <= 5; rows++) {
+	let seatInRow;
+	for (let seat = 1; seat <= 10; seat++) {
+		seatInRow = `${rows}-${seat}`;
+	}
+	console.log(seatInRow);
+}
+
+console.log("Simple Interest Calculation:");
+let principal = 1000;
+let rate = 5;
+let years = 1;
+while ((years = 5)) {
+	let interest = (principal * rate * years) / 100;
+	log.console;
+}
