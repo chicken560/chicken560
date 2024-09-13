8fdf1b7cf1f8d102580943bc5558c663ff147dc9
chicken560 <braydondavis07@gmail.com>
2024-09-13 09:17:47 -0600

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,14 @@
+let Time = {
+	hour: 14,
+	minute: 30,
+	seconds: 45,
+};
+
+console.log(
+	"Current Time:" + Time.hour + ":" + Time.minute + ":" + Time.seconds,
+);
+
+let bankAccount = {
+	accountNumber: "123456789",
+	balance: 5000,
+};
