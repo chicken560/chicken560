5abf22c1c21c21694b7c9770734ce92b7eff1203
chicken560 <braydondavis07@gmail.com>
2024-09-25 09:28:59 -0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+class BankAccount {
+	#accountNumber = "123456";
+	#balance = 1000;
+}
+
+let amount = prompt("How much money do you want to deposit or withdraw?");
+function deposit(amount) {
+	constructor(amount) {
+        this.balance = this.balance - amount
+    } 
+    let money = new BankAccount(``)
+	console.log();
+}
+deposit(amount);
